93de92c2a0ab1ab34d9c73a719029a1ad2e6dde1
saad.aswad <saad.aswad@hiast.edu.sy>
2026-01-04 16:58:26 +0300

    fix: remove --oversubscribe flag

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Running Problem 1: Prefix Sum (NP=3)"
-mpiexec --oversubscribe -n 3 python3 src/problem1/prefix_sum.py
+mpiexec -n 3 python3 src/problem1/prefix_sum.py
 
 echo "-----------------------------------"
 echo "Running Problem 2: Manual Reduce Benchmark (NP=3)"
-mpiexec --oversubscribe -n 3 python3 src/problem2/benchmark.py
+mpiexec -n 3 python3 src/problem2/benchmark.py
